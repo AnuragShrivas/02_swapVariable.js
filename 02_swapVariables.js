0accf3162a38b51f872a3921e73e6e090368a0c0
AnuragShrivas <Anurag.a.shrivas@gmail.com>
2023-05-11 23:13:11 +0530

    swap variables assignment done

---
+++ 02_swapVariables.js
@@ -0,0 +1,25 @@
+
+
+
+
+var sweety = "Sweety";
+var cutie = "Cutie";
+console.log("*****************Before Swap*******************", "sweety:", sweety, " cutie:", cutie);
+var temp = sweety;
+sweety = "Cutie";
+cutie = "Sweety";
+console.log("*****************After Swap********************","Sweety:", sweety, "Cutie:", cutie);
+
+
+
+
+
+
+var num1 = 100;
+var num2 = 200;
+var num3 = 300;
+console.log("*****************Before Swap**************", "Num1:", num1, "Num2:", num2, "Num3", num3);
+num1 = num2;
+num2 = num3;
+num3 = num1;
+console.log("*****************After Swap***************", "Num1:", num1, "Num2:", num2, "Num3:", num3);
